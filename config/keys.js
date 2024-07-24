465065db6823665b3da73c9567971062594cd4b1
Blue Dawn <84982266+inanitynoupcase@users.noreply.github.com>
2024-07-25 01:32:29 +0700

    created project

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+const config = {
+  totalLoop: 15,
+  loop: 10,
+  walletAddress: "YOUR_WALLET_ADDRESS",
+  privateKey: "YOUR_PRIVATE_KEY",
+  contractAddress: "0xa51894664a773981c6c112c43ce576f315d5b1b6", // WETH contract
+  rpcUrl: "https://rpc.mainnet.taiko.xyz",
+  etherAmount: "0.00001",
+};
+module.exports = config;
